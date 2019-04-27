125b15af50ce50ad32ec13cc7d576dc1c66f70eb
EDWIN JACKSON <100138857@student.georgianc.on.ca>
2019-04-27 17:01:53 -0400

    added supporting files

---
+++ src/util/styles.js
@@ -0,0 +1,36 @@
+
+const fonts = {
+  header: {
+    family: "'Lato', sans-serif",
+    sizes: {
+      sm: '1rem',
+      md: '1.5rem',
+      lg: '2rem'
+    }
+  },
+  body: {
+    family: 'Merriweather, Georgia, Times New Roman, Times, serif',
+    size: '1rem'
+  },
+}
+
+const colour = {
+  black: '#232323',
+  diminished: '#23232387',
+  white: '#F2F4F3',
+  gainsboroGrey: '#DCDCDD',
+  yellow: '#FCEB02',
+  blue: '#4392F1',
+}
+
+const spacing = {
+  sm: '0.5rem',
+  md: '1rem',
+  lg: '2rem',
+}
+
+export {
+  fonts,
+  colour,
+  spacing,
+}
