a630377b0de7fcbe6ff1f2c1f730c30d480a45bf
RoisinLocke <locke.roisin@gmail.com>
2019-05-07 22:22:14 -0400

    finsihed duck hero for tablet

--- src/components/blog-hero.js
+++ src/components/blog-hero.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import styled from "styled-components"
-import { Logo } from './base/text'
+import { colour } from '../util/styles'
 
 const Hero = styled.div`
     width: 100%;
-    height: 40%;
-    background-color: #ff7f9d;
+    height: 30vh;
     display: flex;
+    justify-content: center;
+    align-items: center;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-image: url('https://images.unsplash.com/photo-1499334758287-dc8133b315e9?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80')
+`
+
+const Title = styled.h1`
+    color: ${colour.white};
+    text-shadow: 4px 3px ${colour.black};
+    font-family: 'Lato', sans-serif;
+    font-weight: bold;
+    font-size: 2.5rem;
 `
 
 const BlogHero = () => (
     <Hero>
-        <Logo>Latest Posts</Logo>
+        <Title>Need a Rubber Duck? Talk to the blog!</Title>
     </Hero>
 )
 
